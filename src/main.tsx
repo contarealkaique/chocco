186c59d8e2b4e5a099f9a42bc542d928d8404a94
contarealkaique <contarealkaique@gmail.com>
2026-08-10 22:21:05 -0300

    Atualizar o main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,6 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
-
-function App() {
-  return (
-    <main>
-      <h1>CHOCCO</h1>
-      <p>O aplicativo está começando.</p>
-    </main>
-  );
-}
+import App from "./App";
 
 createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
